5109a9f3452b1a4a35978c67c6b17ff92be8af53
Richi Jeffery <kikki_sawn@yahoo.com>
2016-06-24 01:42:20 -0700

    add a default value for reducer so that character starts off without having to do a ternary to see if the stats are there

--- src/reducers/reducer_update-character-stats.js
+++ src/reducers/reducer_update-character-stats.js
@@ -1,7 +1,26 @@
 import * as types from '../constants/action_types';
 import { List, Map, fromJS } from 'immutable';
 
-export default function(state = fromJS([]), action) {
+const DEFAULT = fromJS([{
+  maxHp: 0,
+  currentHp: 1,
+  id: 0,
+  maxMp: 0,
+  currentMp: 0,
+  name: 0,
+  agility: 0,
+  accuracy: 0,
+  str: 0,
+  magic: 0,
+  exp: 0,
+  def: 0,
+  evade: 0,
+  classes: 0,
+  refName: 0,
+  items: 0
+}]);
+
+export default function(state = DEFAULT, action) {
   switch (action.type) {
     case types.UPDATE_CHARACTER_STATS: {
       const character = action.payload;
