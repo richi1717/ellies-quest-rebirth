bfab72b9569764ecba9d7f86d15c4f49eb9ae784
Richi Jeffery <kikki_sawn@yahoo.com>
2016-07-10 01:31:32 -0700

    update logic for damage calculation

--- src/utils/damage-calc.js
+++ src/utils/damage-calc.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 
 export function damageCalculation(power, def, base) {
-  return _.ceil((power * (512 - def) * base) / (1.6 * 512));
+  // return _.ceil((power * (512 - def) * base) / (1.6 * 512));
+  return _.ceil((power * (812 - def) * base) / (1.6 * 512));
   // return 500;
 }
 export function getBaseDamage(str, lvl) {
